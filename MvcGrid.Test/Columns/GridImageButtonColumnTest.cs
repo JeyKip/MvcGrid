794bbc66e20c5d85015b74180a6afbef9ed63099
JeyKip <genyanosolenko@mail.ru>
2014-09-24 19:51:50 +0300

    GridImageButtonColumn was added

---
+++ MvcGrid.Test/Columns/GridImageButtonColumnTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace MvcGrid.Test.Columns
+{
+    [TestFixture]
+    public class GridImageButtonColumnTest : GridColumnBaseTest<GridImageButtonColumn>
+    {
+        [Test]
+        public void ToString_EmptyFunctionName_EmptyString()
+        {
+            GridImageButtonColumn ibc = new GridImageButtonColumn();
+
+            string expected = string.Empty;
+            string actual = ibc.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToString_FunctionNameIsSetted_CorrectFormatter()
+        {
+            GridImageButtonColumn column = new GridImageButtonColumn();
+            column.SetClickHandler("clickHandler");
+            column.SetIconClass("ui-icon-pencil");
+            column.SetToolTip("Some title");
+
+            string expected =
+@"formatter: function (cv,o,ro){
+    var cf = ""\""clickHandler(this);\"""";
+    var ocl = ""onclick=""+cf+"" onmouseover=jQuery(this).addClass('ui-state-hover'); onmouseout=jQuery(this).removeClass('ui-state-hover') "";
+    var str = ""<div title='"" + ""Some title"" + ""' style='float:left;cursor:pointer;' class='ui-pg-div ui-inline-edit' "" + ocl + ""><span class='ui-icon ui-icon-pencil'></span></div>"";
+    return ""<div>"" + str + ""</div>"";
+}".RemoveSpaces();
+
+            string actual = column.ToString().RemoveSpaces();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToString_FunctionNameWithParams_CorrectFormatter()
+        {
+            GridImageButtonColumn column = new GridImageButtonColumn();
+            column.SetClickHandler("clickHandler", "ID", "Description", "Status");
+            column.SetIconClass("ui-icon-pencil");
+            column.SetToolTip("Some title");
+
+            string expected =
+@"formatter: function (cv,o,ro){
+    var cf = ""\""clickHandler(this, '""+ro[""ID""]+""', '""+ro[""Description""]+""', '""+ro[""Status""]+""');\"""";
+    var ocl = ""onclick=""+cf+"" onmouseover=jQuery(this).addClass('ui-state-hover'); onmouseout=jQuery(this).removeClass('ui-state-hover') "";
+    var str = ""<div title='"" + ""Some title"" + ""' style='float:left;cursor:pointer;' class='ui-pg-div ui-inline-edit' "" + ocl + ""><span class='ui-icon ui-icon-pencil'></span></div>"";
+    return ""<div>"" + str + ""</div>"";
+}".RemoveSpaces();
+
+            string actual = column.ToString().RemoveSpaces();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToString_FunctionNameWithDisplayCondition_CorrectFormatter()
+        {
+            GridImageButtonColumn column = new GridImageButtonColumn();
+            column.SetClickHandler("clickHandler", "ID", "Description", "Status");
+            column.SetIconClass("ui-icon-pencil");
+            column.SetToolTip("Some title");
+            column.DisplayWhen(x => x["Status"] == "1");
+
+            string expected =
+@"formatter: function (cv,o,ro){
+    if ((ro[""Status""] == ""1"")){
+        var cf = ""\""clickHandler(this, '""+ro[""ID""]+""', '""+ro[""Description""]+""', '""+ro[""Status""]+""');\"""";
+        var ocl = ""onclick=""+cf+"" onmouseover=jQuery(this).addClass('ui-state-hover'); onmouseout=jQuery(this).removeClass('ui-state-hover') "";
+        var str = ""<div title='"" + ""Some title"" + ""' style='float:left;cursor:pointer;' class='ui-pg-div ui-inline-edit' "" + ocl + ""><span class='ui-icon ui-icon-pencil'></span></div>"";
+        return ""<div>"" + str + ""</div>"";
+    }
+    return """";
+}".RemoveSpaces();
+
+            string actual = column.ToString().RemoveSpaces();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- MvcGrid/Columns/GridImageButtonColumn.cs
+++ MvcGrid/Columns/GridImageButtonColumn.cs
@@ -1,16 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using MvcGrid.Utilites;
 
-namespace MvcGrid.Columns
+namespace MvcGrid
 {
-    public class GridImageButtonColumn : GridColumnBase<GridImageButtonColumn>
+    public class GridImageButtonColumn : GridColumnBase<GridImageButtonColumn>, ICustomFormatterColumn
     {
+        private Expression<Func<FilterCondition, bool>> _displayCondition = null;
+        private string _clickFunction = null;
+        private string _toolTip = null;
+        private string _iconClass = null;
+        private string[] _clickHandlerParams = null;
+
+        public GridImageButtonColumn DisplayWhen(Expression<Func<FilterCondition, bool>> displayCondition)
+        {
+            _displayCondition = displayCondition;
+            return GetInstance();
+        }
+
+        /// <summary>
+        /// Set a name of click event handler
+        /// </summary>
+        public GridImageButtonColumn SetClickHandler(string clickFunctionName, params string[] args)
+        {
+            _clickFunction = clickFunctionName;
+            _clickHandlerParams = args;
+            return GetInstance();
+        }
+
+        public GridImageButtonColumn SetToolTip(string toolTip)
+        {
+            _toolTip = toolTip;
+            return GetInstance();
+        }
+
+        public GridImageButtonColumn SetIconClass(string iconClass)
+        {
+            _iconClass = iconClass;
+            return GetInstance();
+        }
+
+        #region Overrides
         protected override GridImageButtonColumn GetInstance()
         {
             return this;
         }
+
+        public override string ToString()
+        {
+            if (GetProperty("formatter") == null)
+            {
+                if (!string.IsNullOrWhiteSpace(GetFormatter()))
+                    AddProperty("formatter", GetFormatter());
+            }
+
+            return base.ToString();
+        }
+
+        protected override string ResolveProperty(KeyValuePair<string, object> x)
+        {
+            if (x.Key == "formatter")
+                return string.Format("{0}: {1}", x.Key, x.Value);
+
+            return base.ResolveProperty(x);
+        }
+
+        public string GetFormatter()
+        {
+            if (string.IsNullOrWhiteSpace(_clickFunction))
+                return string.Empty;
+
+            string _clickHandler = string.Empty;
+            if (_clickHandlerParams != null && _clickHandlerParams.Length > 0)
+                _clickHandler = string.Format("{0}(this,{1});", _clickFunction, string.Join(",", _clickHandlerParams.Select(x => string.Format(@"'""+ro[""{0}""]+""'", x))));
+            else
+                _clickHandler = string.Format("{0}(this);", _clickFunction);
+
+            return string.Format(
+@"function (cv,o,ro){{
+    {1}
+    var cf = ""\""{0}\"""";
+    var ocl = ""onclick=""+cf+"" onmouseover=jQuery(this).addClass('ui-state-hover'); onmouseout=jQuery(this).removeClass('ui-state-hover') "";
+    var str = ""<div title='"" + ""{3}"" + ""' style='float:left;cursor:pointer;' class='ui-pg-div ui-inline-edit' "" + ocl + ""><span class='ui-icon {4}'></span></div>"";
+    return ""<div>"" + str + ""</div>"";
+    {2}
+}}", _clickHandler, _displayCondition == null ? "" : string.Format("if ({0}) {{\n", FilterConditionToStringConvertor.Convert(_displayCondition)),
+     _displayCondition == null ? "" : 
+@"}
+return """";",
+     _toolTip, _iconClass);
+        }
+        #endregion
     }
 }
